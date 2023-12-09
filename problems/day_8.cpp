2423e7ba6c07506a251ffafa3769da54e4341b02
maslabgamer <maslabgamer@gmail.com>
2023-12-08 16:13:03 -0800

    Little more optimization
    Reserves space for unordered map beyond map nodes in file
    
    Inlines some variables (not actually an optimization, I just prefer it this way)
    
    Inlines maze linkup with node creation
      Removes node pointer stack as a result since we no longer need it to link up the maze
    
    Removes left and right strings from maze_node
    
    Removes add_node and get_node in favor of direct indexing into the map for inlined node creation and lookup
    
    modify run_maze to use out variables instead of returning a value based on flag
      Runs parts 1 and 2 at the same time with the same map as a result
    
    Removes maze_node constructors
      This is a struct anyway, don't actually have too much logic here
    
    Removes GHOST_COUNT
      Was only used once. Fear my magic numbers.

--- problems/day_8.cpp
+++ problems/day_8.cpp
@@ -9,35 +9,20 @@
 
 #include <vector>
 #include <unordered_map>
-#include <stack>
 #include <numeric>
 
 static constexpr int INSTRUCTION_COUNT = 281;
-static constexpr int GHOST_COUNT = 6;
 
 struct maze_node {
     std::string current_pos;
-    std::string left_chars;
-    std::string right_chars;
-    maze_node* left{};
-    maze_node* right{};
+    maze_node* left = nullptr;
+    maze_node* right = nullptr;
 
-    maze_node() {
-        left = nullptr;
-        right = nullptr;
-    }
+    [[nodiscard]] inline bool is_start_node() const { return current_pos[2] == 'A'; }
 
-    explicit maze_node(const char* line) : maze_node() {
-        current_pos.assign(line, 0, 3);
-        left_chars.assign(line, 7, 3);
-        right_chars.assign(line, 12, 3);
-    }
-
-    bool is_start_node() { return current_pos[2] == 'A'; }
-
-    bool ends_with(const char* ending) {
+    inline bool ends_with(const char* ending) const {
         int current_pos_idx = 2;
-        while (*ending != NULL_TERMINATOR && current_pos_idx >= 0) {
+        while (*ending != NULL_TERMINATOR) {
             if (current_pos[current_pos_idx] != *ending) {
                 return false;
             }
@@ -52,37 +37,23 @@ class maze_holder {
 public:
     explicit maze_holder(std::vector<std::string> data) {
         instructions = data[0];
-
-        std::stack<maze_node*> maze_stack;
+        maze.reserve(750);
 
         int ghost_counter = 0;
 
         for (int line_idx = 2; line_idx < data.size(); line_idx++) {
-            maze_node* new_node = add_node(data[line_idx].data());
-            maze_stack.push(new_node);
+            std::string key(data[line_idx], 0, 3);
+
+            maze_node* new_node = &maze[key];
+            new_node->current_pos = key;
+            new_node->left = &maze[std::string(data[line_idx], 7, 3)];
+            new_node->right = &maze[std::string(data[line_idx], 12, 3)];
 
             if (new_node->is_start_node()) {
-                ghost_paths[ghost_counter] = new_node;
+                ghost_paths[ghost_counter] = key;
                 ghost_counter++;
             }
         }
-
-        while (!maze_stack.empty()) {
-            maze_node* current_node = maze_stack.top();
-            current_node->left = get_node(current_node->left_chars);
-            current_node->right = get_node(current_node->right_chars);
-            maze_stack.pop();
-        }
-    }
-
-    maze_node* add_node(char* line) {
-        std::string key(line, 0, 3);
-        auto it = maze.emplace(key, line);
-        return &it.first->second;
-    }
-
-    maze_node* get_node(std::string& to_get) {
-        return &maze[to_get];
     }
 
     int find_path_to_node(
@@ -102,14 +73,9 @@ public:
     }
 
     unsigned long process_ghost_paths() {
-        unsigned long ghost_steps_taken[6];
-        for (int ghost_idx = 0; ghost_idx < GHOST_COUNT; ghost_idx++) {
-            ghost_steps_taken[ghost_idx] = find_path_to_node(ghost_paths[ghost_idx]->current_pos, "Z");
-        }
-
-        unsigned long steps_count = ghost_steps_taken[0];
-        for (int ghost_idx = 1; ghost_idx < GHOST_COUNT; ghost_idx++) {
-            steps_count = std::lcm(steps_count, ghost_steps_taken[ghost_idx]);
+        unsigned long steps_count = 1;
+        for (const auto & ghost_path : ghost_paths) {
+            steps_count = std::lcm(steps_count, find_path_to_node(ghost_path, "Z"));
         }
         return steps_count;
     }
@@ -117,20 +83,12 @@ public:
 private:
     std::unordered_map<std::string, maze_node> maze;
     std::string instructions;
-
-    maze_node* ghost_paths[GHOST_COUNT]{};
+    std::string ghost_paths[6]{};
 };
 
-unsigned long run_maze(maze_holder& maze, bool ghost_traveling = false) {
-    unsigned long steps_count;
-    if (ghost_traveling) {
-        steps_count = maze.process_ghost_paths();
-    } else {
-        std::string start_str("AAA");
-        steps_count = maze.find_path_to_node(start_str, "ZZZ");
-    }
-
-    return steps_count;
+void run_maze(maze_holder& maze, unsigned long* out_main_path_steps, unsigned long* out_ghost_steps) {
+    *out_main_path_steps = maze.find_path_to_node(std::string("AAA"), "ZZZ");
+    *out_ghost_steps = maze.process_ghost_paths();
 }
 
 void day_8::run() {
@@ -140,8 +98,8 @@ void day_8::run() {
     printf("Day 8\n");
     clock_t tStart = clock();
     maze_holder maze(input_file);
-    unsigned long steps_taken = run_maze(maze);
-    unsigned long ghost_steps_taken = run_maze(maze, true);
+    unsigned long steps_taken, ghost_steps_taken;
+    run_maze(maze, &steps_taken, &ghost_steps_taken);
     printf("Time taken: %.10fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
     printf("Part 1: %lu and the answer was 13207!\n", steps_taken);
     printf("Part 2: %lu and the answer was 12324145107121!\n", ghost_steps_taken);
